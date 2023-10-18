5d211a7b42824ddd909d108503871af8582e6101
Erkan Mustafa <erknm.ckr@gmail.com>
2023-10-18 17:35:59 +0300

    We have made the templates more usable with include blocks and mixes used in pug pages.

--- express/app.js
+++ express/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoutes from './routes/admin.js'
 import path from 'path';
-
+import { products } from './routes/admin.js';
 const app = express();
 const port = 4000;
 
@@ -16,22 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 
-app.get("/about", (req, res, next) => {
-  console.log("Request Type:", req.method);
-  res.send("Welcome to the about page");
-});
-
 // public klasorunu dısa aktardık.
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get("/", (req, res, next) => {
-  const products = [
-    {name:"Iphone 13 Pro Max",price:"30000",description:"Granit",image:"/iphone13.jpeg"},
-    {name:"Iphone 13 Pro Max",price:"30000",description:"Granit",image:"/iphone13.jpeg"},
-    {name:"Iphone 13 Pro Max",price:"30000",description:"Granit",image:"/iphone13.jpeg"},
-    {name:"Iphone 13 Pro Max",price:"30000",description:"Granit",image:"/iphone13.jpeg"},
-  ]
-  res.render(path.join(__dirname,'pages','index'),{title:'Home Page',products:products})
+  res.render('index',{title:'Home Page',products:products})
 });
 
 
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -3,15 +3,46 @@ const app = express();
 import path from "path";
 
 const router = express.Router();
+export const products = [
+  {
+    name: "Iphone 13 Pro Max",
+    price: "30000",
+    description: "Granit",
+    image: "/iphone13.jpeg",
+  },
+  {
+    name: "Iphone 13 Pro Max",
+    price: "30000",
+    description: "Granit",
+    image: "/iphone13.jpeg",
+  },
+  {
+    name: "Iphone 13 Pro Max",
+    price: "30000",
+    description: "Granit",
+    image: "/iphone13.jpeg",
+  },
+  {
+    name: "Iphone 13 Pro Max",
+    price: "30000",
+    description: "Granit",
+    image: "/iphone13.jpeg",
+  },
+];
 
 // add-product sayfasına get ıstegı oldugu zaman add-product.html sayfasını yolluyoruz.
-router.get('/add-product',(req,res,next)=>{
-    res.render('add-product',{title:'Add Product Page'})
-})
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", { title: "Add Product Page" });
+});
 
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
+router.post("/add-product", (req, res, next) => {
+  products.push({
+    name: req.body.name,
+    price: req.body.rice,
+    description: req.body.description,
+    image: req.body.image,
+  });
+  res.redirect("/");
+});
 
 export default router;
